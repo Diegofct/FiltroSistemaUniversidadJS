9cae0a61f8a738085612321661db56ec635ad503
Diegofct <96diegofct@gmail.com>
2024-03-05 22:01:21 -0500

    avanzando en el diseño del proyecto

--- js/componentes.js
+++ js/componentes.js
@@ -1,6 +1,9 @@
+const main = document.getElementById('contenido-principal')
 const botonesMenu = document.querySelectorAll(".boton-menu")
 const seccionInicio = document.getElementById('seccion-inicio')
 const seccionNosotros = document.getElementById('seccion-nosotros')
+const seccionContacto = document.getElementById('seccion-contactame')
+const departamentos = document.getElementById('departamentos')
 
 const cargarSeccionInicio = () => {
     seccionInicio.innerHTML = `
@@ -14,6 +17,29 @@ const cargarSeccionInicio = () => {
     `
 }
 
+const cargarSeccionNosotros = () => {
+    seccionNosotros.innerHTML = `
+
+        <div class="contenedor-nosotros">
+            <h1 class="titulo">¿Quienes somos?</h1>
+            <h3 class="texto-h3">Universidad de los Sabios </h3>
+            <p class="descripcion">Una institución líder en conocimiento y sabiduría, comprometida con la excelencia académica y la innovación.</p>
+        </div>
+    
+    `
+}
+
+const cargarSeccionContactame = () => {
+    seccionContacto.innerHTML = `
+
+    <div class="contenedor-contacto">
+        <h1 class="titulo">Contactanos</h1>
+        <h3 class="texto-h3">Universidad de los Sabios </h3>
+        <p class="descripcion">Una institución líder en conocimiento y sabiduría, comprometida con la excelencia académica y la innovación.</p>
+    </div>
+
+    `
+}
 
 
 botonesMenu.forEach(boton => {
@@ -22,10 +48,14 @@ botonesMenu.forEach(boton => {
         e.currentTarget.classList.add("active");
 
         if (e.currentTarget.id === "inicio") {
-            cargarSeccionInicio()
             seccionInicio.style.display = "grid"
+            seccionNosotros.style.display = "flex"
+            seccionContacto.style.display = "flex"
+
         } else if (e.currentTarget.id === "departamentos") {
-            seccionInicio.style.display = "none"
+            seccionInicio.style.display = 'none'
+            seccionNosotros.style.display = 'none'
+            seccionContacto.style.display = 'none'
         }
 
     })
